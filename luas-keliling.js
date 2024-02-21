3e69fef2e05fc1bd9a7e1e9f3732502a39747dd1
Ikram Syawal Alitu <ikramsyawalalitu@gmail.com>
2024-02-21 21:13:32 +0800

    create 4 func: luas & keliling

---
+++ luas-keliling.js
@@ -0,0 +1,15 @@
+exports.luasPersegi = (sisi) => {
+    return sisi * sisi
+}
+
+exports.kelilingPersegi = (sisi) => {
+    return 4 * sisi
+}
+
+exports.luasPersegiPanjang = (panjang, lebar) => {
+    return panjang * lebar
+}
+
+exports.kelilingPersegiPanjang = (panjang, lebar) => {
+    return 2 * (panjang + lebar)
+}
